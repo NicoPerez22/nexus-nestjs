88f992a6cae2464887ecf9500792a401f60e10ee
Nico Perez <ZEXTNAPerez@correoargentino.com.ar>
2026-09-09 15:45:51 -0300

    Primer commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { APP_GUARD } from "@nestjs/core";
 import { ThrottlerModule, ThrottlerGuard } from "@nestjs/throttler";
-import { databaseOptions } from "./database/data-source";
 import { AuthGuard } from "./common/auth.guard";
 import { AuthModule } from "./feature/auth/auth.module";
 import { TeamsModule } from "./feature/teams/teams.module";
@@ -10,9 +9,29 @@ import { CalendarModule } from "./feature/calendar/calendar.module";
 import { TasksModule } from "./feature/tasks/tasks.module";
 import { DashboardModule } from "./feature/dashboard/dashboard.module";
 import { OrganizationModule } from "./feature/organization/organization.module";
+
 @Module({
   imports: [
-    TypeOrmModule.forRoot(databaseOptions),
+    TypeOrmModule.forRoot(
+      {
+      type: 'mysql',
+      host: process.env.MYSQLHOST,
+      port: Number(process.env.MYSQLPORT || 3306),
+      username: process.env.MYSQLUSER,
+      password: process.env.MYSQLPASSWORD,
+      database: process.env.MYSQLDATABASE,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: false,
+      autoLoadEntities: true,
+      extra: {
+        connectionLimit: 10, // máximo 10 conexiones vivas
+        waitForConnections: true, // no lanzar error, poner en cola
+        queueLimit: 0, // sin límite de cola
+        connectTimeout: 10000, // 10 segundos
+        acquireTimeout: 10000, // timeout para adquirir conexión
+      },
+    }
+    ),
     ThrottlerModule.forRoot([{ ttl: 60000, limit: 120 }]),
     OrganizationModule,
     AuthModule,
